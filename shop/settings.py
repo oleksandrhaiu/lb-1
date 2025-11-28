ca336871e49d3569a608b9a4cb8d2b9c72fadad8
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-28 19:48:39 +0200

    build(task 9): enable debug mode

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xzgoj@o6x=ya05p46_i6kz9w2um)2d1*)xuefmsvnv19hy14i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
