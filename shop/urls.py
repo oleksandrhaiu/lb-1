27ad3cbf86969c134682272203cfd0f74c2c276a
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-28 21:03:54 +0200

    feat(task 16): clone products app to orders and configure routes

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include # 1. Додано include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')), # 2. Підключено файл маршрутів додатку
+    path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),  # Новий маршрут
 ]
