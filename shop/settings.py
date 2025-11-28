45bc99bcba4a06bd4cf5de99e679adea751006f9
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-28 19:42:24 +0200

    build(task 8): disable debug mode and update allowed hosts

--- shop/settings.py
+++ shop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xzgoj@o6x=ya05p46_i6kz9w2um)2d1*)xuefmsvnv19hy14i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
