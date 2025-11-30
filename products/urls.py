f8950ffa080348a46141ef18337bd339b4519d88
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-30 21:57:55 +0200

    feat(lab3): configure urls for categories and products pages

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # Маршрут для категорій: /products/categories/
+    path('categories/', views.categories, name='categories'),
+
+    # Маршрут для товарів: /products/products/
+    path('products/', views.products, name='products'),
 ]
