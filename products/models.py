8c04d433237d2ea31a71f378d29c0bfcf5e017bc
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-30 17:11:28 +0200

    feat(task 12): implement Category and Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    product_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
