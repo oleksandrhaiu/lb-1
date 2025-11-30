a6828e2b1adfddbce335ac01388c8bfcf4557314
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-30 17:52:56 +0200

    feat(task 22): add products list to index view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
-from .models import Category
+from .models import Category, Product
+
 
 def index(request):
+    # Отримуємо дані з бази
     categories = Category.objects.all()
-    return HttpResponse(''.join([str(category) + '<br>' for category in categories]))
+    products = Product.objects.all()
+
+    # Формуємо рядок відповіді
+    response_html = "<h1>Категорії:</h1>"
+    response_html += ''.join([f"- {c.title}<br>" for c in categories])
+
+    response_html += "<h1>Товари:</h1>"
+    response_html += ''.join([f"- {p.title} (Ціна: {p.price})<br>" for p in products])
+
+    return HttpResponse(response_html)
