47655497b8dc4f03e0dfe008cc41352b736248e0
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-30 17:30:15 +0200

    feat(task 16): register Category and Product in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# Реєстрація моделей в адмінці
+admin.site.register(Category)
+admin.site.register(Product)
