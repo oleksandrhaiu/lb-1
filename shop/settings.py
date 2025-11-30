3f0e6f2c9556761e5a7f47f6069af54c4fcee8bd
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-30 16:57:12 +0200

    build(task 11): ensure products app is installed

--- shop/settings.py
+++ shop/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
-    'orders',  # Новий додаток
+    'products', # Додаток
+    'orders',
 ]
 
 MIDDLEWARE = [
