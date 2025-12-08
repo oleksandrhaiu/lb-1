7b7f0f80b45b3a95e8ca434a3801cc7a3507589d
Oleksadr Haiu <sashagayu1@gmail.com>
2025-12-08 15:15:25 +0200

    feat(lab4): implement CreateView for products and categories

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.CategoryListView.as_view(), name='categories'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category-create'),  # Новий
+
     path('products/', views.ProductListView.as_view(), name='products'),
+    path('products/create/', views.ProductCreateView.as_view(), name='product-create'),  # Новий
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView  # Додали CreateView
+from django.urls import reverse_lazy  # Для перенаправлення після успіху
 from .models import Product, Category
+from .forms import ProductForm, CategoryForm  # Імпорт форм
+
 
 class CategoryListView(ListView):
     model = Category
@@ -8,9 +11,24 @@ class CategoryListView(ListView):
     context_object_name = 'categories'
     ordering = ['title']
 
+
 class ProductListView(ListView):
     model = Product
     template_name = 'products/products.html'
     context_object_name = 'products'
     paginate_by = 5  # Кількість товарів на одній сторінці (для тесту)
     ordering = ['-created_at']
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'products/product_form.html'  # Використаємо один шаблон для обох
+    success_url = reverse_lazy('categories')  # Куди повертати після створення
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('products')
