899a0556c268e6309196c56a085624d61f5043d9
Oleksadr Haiu <sashagayu1@gmail.com>
2025-12-08 16:18:17 +0200

    feat(lab4): add Meta class for localization and ordering

--- products/models.py
+++ products/models.py
@@ -7,13 +7,25 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    # --- НОВЕ: Налаштування відображення ---
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+        ordering = ['title']
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     price = models.FloatField()
     product_qty = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    # --- НОВЕ: Налаштування відображення ---
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товари'
+        ordering = ['title']
