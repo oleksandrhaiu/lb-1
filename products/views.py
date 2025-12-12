fc5ca48476e345381d52091ed9308ec7eadcf303
Oleksadr Haiu <sashagayu1@gmail.com>
2025-12-12 15:07:21 +0200

    feat(lab5): protect CRUD operations with LoginRequiredMixin

--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView # <--- Додано DeleteView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+# 1. Імпорт міксина для захисту
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Product, Category
 from .forms import ProductForm, CategoryForm
 
+# --- READ (Перегляд доступний всім) ---
 
 class CategoryListView(ListView):
     model = Category
@@ -11,40 +15,45 @@ class CategoryListView(ListView):
     context_object_name = 'categories'
     ordering = ['title']
 
-
 class ProductListView(ListView):
     model = Product
     template_name = 'products/products.html'
     context_object_name = 'products'
-    paginate_by = 5  # Кількість товарів на одній сторінці (для тесту)
+    paginate_by = 5
     ordering = ['-created_at']
-
-
-class CategoryCreateView(CreateView):
-    model = Category
-    form_class = CategoryForm
-    template_name = 'products/product_form.html'  # Використаємо один шаблон для обох
-    success_url = reverse_lazy('categories')  # Куди повертати після створення
-
-
-class ProductCreateView(CreateView):
-    model = Product
-    form_class = ProductForm
-    template_name = 'products/product_form.html'
-    success_url = reverse_lazy('products')
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
 
-class ProductUpdateView(UpdateView):
+# --- CREATE, UPDATE, DELETE (Тільки для авторизованих) ---
+
+# Додаємо LoginRequiredMixin першим у дужках!
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('categories')
+    login_url = '/login/'  # Куди перенаправляти, якщо не увійшов
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
-    template_name = 'products/product_form.html'  # Використовуємо той самий шаблон, що і для створення
+    template_name = 'products/product_form.html'
     success_url = reverse_lazy('products')
+    login_url = '/login/'
 
-class ProductDeleteView(DeleteView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('products')
+    login_url = '/login/'
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'products/product_confirm_delete.html'
-    success_url = reverse_lazy('products') # Куди повертати після видалення
+    success_url = reverse_lazy('products')
+    login_url = '/login/'
