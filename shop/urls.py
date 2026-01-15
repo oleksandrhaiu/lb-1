6d37409dbd813f9fe8ddeb7654a919f27f2b47e3
Oleksadr Haiu <sashagayu1@gmail.com>
2026-01-15 14:35:04 +0200

    feat(lab6): include cart urls in main project

--- shop/urls.py
+++ shop/urls.py
@@ -5,9 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('orders/', include('orders.urls')),  # Новий маршрут
+    path('orders/', include('orders.urls')),
 
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
